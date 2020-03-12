0de85d5f2558149f667d0a44c89b3f8356eec67f
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2020-03-12 22:27:18 +0200

    Set indent_style=space

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const logger = require('./logger');
 const app = express();
 
 const courses = [
-	{ id: 1, name: 'course1' },
-	{ id: 2, name: 'course2' },
-	{ id: 3, name: 'course3' },
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
 ]
 
 app.use(express.json());
@@ -20,72 +20,72 @@ app.use(morgan());
 app.use(logger)
 
 app.get('/', (req, res) => {
-	res.send('Hello world!!!');
+  res.send('Hello world!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-	res.send(courses);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-	const course = courses.find((c) => c.id === parseInt(req.params.id, 10));
-	if (!course) {
-		res.status(404).send('The course with given ID does not exist');
-	}
+  const course = courses.find((c) => c.id === parseInt(req.params.id, 10));
+  if (!course) {
+    res.status(404).send('The course with given ID does not exist');
+  }
 
-	res.send(course);
+  res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
-	const { error } = validateCourse(req.body)
-	if (error) {
-		return res.status(400).send(error.details[0].message)
-	}
+  const { error } = validateCourse(req.body)
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  }
 
-	const course = {
-		id: courses.length + 1,
-		name: req.body.name
-	}
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
 
-	courses.push(course);
-	res.send(course);
+  courses.push(course);
+  res.send(course);
 });
 
 app.put('/api/courses/:id', (req, res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) {
-		return res.status(404).send('The course with given ID does not exist');
-	}
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('The course with given ID does not exist');
+  }
 
-	const { error } = validateCourse(req.body)
-	if (error) {
-		res.status(400).send(error.details[0].message)
-	}
+  const { error } = validateCourse(req.body)
+  if (error) {
+    res.status(400).send(error.details[0].message)
+  }
 
-	course.name = req.body.name;
-	res.send(course);
+  course.name = req.body.name;
+  res.send(course);
 });
 
 app.delete('/api/courses/:id', (req, res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) {
-		return res.status(404).send('The course with given ID does not exist');
-	}
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('The course with given ID does not exist');
+  }
 
-	const index = courses.indexOf(course);
-	courses.splice(index, 1);
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
 
-	res.send(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening ${port}`));
 
 const validateCourse = (course) => {
-	const schema = {
-		name: Joi.string().min(3).required()
-	}
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
 
-	const result = Joi.validate(course, schema);
-	return result;
+  const result = Joi.validate(course, schema);
+  return result;
 }
