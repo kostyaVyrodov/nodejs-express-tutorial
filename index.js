d26ee88c347be5fdac06f10553762f9ac898898b
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2020-03-13 22:40:43 +0200

    Restructure application

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const dbDebugger = require('debug')('app:db');
 
 const config = require('config');
 const express = require('express');
-const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const logger = require('./logger');
+const home = require('./routes/home');
+const courses = require('./routes/courses');
+const genres = require('./routes/genres');
+const logger = require('./middleware/logger');
 
 const app = express();
 
@@ -17,12 +19,10 @@ console.log('Application name: ' + config.get('name'));
 console.log('Mail server: ' + config.get('mail.host'));
 console.log('Mail server password: ' + config.get('mail.password'));
 
-
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' },
-]
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  startupDebugger('Morgan enabled...');
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,78 +33,10 @@ app.use(logger);
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-if (app.get('env') === 'development') {
-  app.use(morgan('tiny'));
-  startupDebugger('Morgan enabled...');
-}
+app.use('/api/courses', courses);
+app.use('/api/genres', genres);
+app.use('/', home)
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'My Express App', message: 'hello' })
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id, 10));
-  if (!course) {
-    res.status(404).send('The course with given ID does not exist');
-  }
-
-  res.send(course);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body)
-  if (error) {
-    return res.status(400).send(error.details[0].message)
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  }
-
-  courses.push(course);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(404).send('The course with given ID does not exist');
-  }
-
-  const { error } = validateCourse(req.body)
-  if (error) {
-    res.status(400).send(error.details[0].message)
-  }
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(404).send('The course with given ID does not exist');
-  }
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-});
-
-const validateCourse = (course) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
-
-  const result = Joi.validate(course, schema);
-  return result;
-}
 
 dbDebugger('Connected to the database...')
 
